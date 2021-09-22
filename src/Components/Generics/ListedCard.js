19875a42fc88c22d24c14a25d057c2fd1e58b6fa
adelowo ajibola <adelowomi@gmail.com>
2021-09-22 03:16:02 +0100

    fixed modal

--- src/Components/Generics/ListedCard.js
+++ src/Components/Generics/ListedCard.js
@@ -29,7 +29,7 @@ export default function ListedCard({ property = {}, seeMore, isProperty, request
     return (
         <>
         <Modal
-				open={seeMore}
+				open={seeeMore}
 				onClose={() => {
 					setSeeeMore(false);
 				}}
