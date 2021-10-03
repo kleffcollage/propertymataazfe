8c75f1bd91431525711da81a6ae8c10fb7ef3b72
ayodejipy <ay.programmr@gmail.com>
2021-10-03 18:02:42 +0100

    Made some changes to sessionCard

--- src/Components/Generics/SessionsCard.js
+++ src/Components/Generics/SessionsCard.js
@@ -8,143 +8,34 @@ import RentReliefDetails from '../../Pages/Rent/RentReliefDetails';
 import RequestResults from '../../Pages/Rent/RequestResults';
 import TenancyDetails from '../../Pages/Rent/Landlord/TenancyDetails';
 
-export default function SessionsCard({ property = {}, }) {
+export default function SessionsCard({ data = {}, }) {
     const [ cancelModal, setCancelModal ] = useState(false)
     const [ openDetails, setOpenDetails ] = useState(false)
-    const [ tenancyDetails, setTenancyDetails ] = useState(false)
-    const [ requestResults, setRequestResults ] = useState(false)
     const [ propertyId, setPropertyId ] = useState('')
     
     
     const openRentDetails = () => {
         setOpenDetails(prev => !prev)
     }
-    
-    const openTenancyDetails = () => {
-        setTenancyDetails(prev => !prev)
-    }
-    
-    const toggleRequests = (id) => {
-        setPropertyId(id)
-    }
-    const openCancel = () => {
-        setCancelModal(prev => !prev)
-    }
-    
     // console.log({property})
 
     return (
         <>
-        <Alert 
-            showAlert={cancelModal} setShowAlert={setCancelModal} isCancel={true}     
-        />
+            <Alert 
+                showAlert={cancelModal} setShowAlert={setCancelModal} isCancel={true}     
+            />
+            
+            {/* Tenants rental details */}
+            
+            {/* <Modal open={tenancyDetails} onClose={() => setTenancyDetails(false)}>
+                <TenancyDetails isTenant={true} propertyId={property.id} close={() => setTenancyDetails(false)} />
+            </Modal> */}
         
-        {/* Rent relief Modal */}
-        <Modal open={openDetails} onClose={() => setOpenDetails(false)}>
-            <RentReliefDetails close={() => setOpenDetails(false)} />
-        </Modal>
-        
-        {/* Property request Modal */}
-        <Modal open={requestResults} onClose={() => setRequestResults(false)}>
-            <RequestResults propertyId={propertyId} matches={property.matches} theRequest={property.comment} close={() => setRequestResults(false)} />
-        </Modal>
-        
-        {/* Tenants rental details */}
-        <Modal open={tenancyDetails} onClose={() => setTenancyDetails(false)}>
-            <TenancyDetails isTenant={true} propertyId={property.id} close={() => setTenancyDetails(false)} />
-        </Modal>
-        
-        { isRequest  ? (
             <div className="col-lg-4">
                 <div className="listing-cards for-request pt-4">
                     <div className="listing-info for-request">
                         <div className="title-group">
-                            <div className="listing-title mb-3">{property.comment}</div>
-                        </div>
-                        <div className="feature-group">
-                            <div className="feature-sing">
-                                <i className="far fa-bed" />
-                                <div className="feature-title">
-                                    {`${property.numberOfBedRooms} Bedrooms`}
-                                </div>
-                            </div>
-                            <div className="feature-sing">
-                                <i className="far fa-toilet" />
-                                <div className="feature-title">
-                                    {`${property.numberOfBathrooms} Bathrooms`}
-                                </div>
-                            </div>
-                            <div className="feature-sing">
-                                <i className="far fa-tags" />
-                                <div className="feature-title"><Naira>{property.budget}</Naira></div>
-                            </div>
-                            <div className="feature-sing">
-                                <i className="far fa-award" />
-                                <div className="feature-title">
-                                    { property.propertyType.name.toLowerCase()}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* <div className="line" /> */}
-                    <div className="listing-info pt-0">
-                        <div className="listing-btn">
-                            <button className="list-no-color-btn" 
-                                onClick={openCancel}
-                            > 
-                                Cancel
-                            </button>
-                                
-                            <button
-                                className="list-color-btn"
-                                onClick={() => {
-                                    toggleRequests(5);
-                                    setRequestResults(true);
-                                }}
-                                disabled={property.matches.length == 0}
-                            >
-                                { property.matches.length == 0 
-                                    ? 'No matches yet' 
-                                    : `View ${property.matches.length} Matches` }
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        ): isRelief ? (
-            <div className="col-lg-4">
-                <div className="listing-cards for-relief pt-4" onClick={openRentDetails}>
-                    <div className="listing-info">
-                        <div className="title-group align-items-center mb-4">
-                            <div className="relief-amount">
-                                <h6>Relief Amount</h6>
-                                <p className="mb-0"><Naira>4500000</Naira></p>
-                            </div>
-                            <div className="loan-status">Pending</div>                            
-                        </div>
-                        <div className="d-flex justify-content-between">
-                            <div className="relief-amount">
-                                <h6>Interest</h6>
-                                <p className="mb-0">15%</p>
-                            </div>
-                            <div className="relief-amount">
-                                <h6>Monthly Instalments</h6>
-                                <p className="mb-0"><Naira>797062</Naira></p>
-                            </div>
-                            <div className="relief-amount">
-                                <h6>Total Repayment</h6>
-                                <p className="mb-0"><Naira>4782372</Naira></p>
-                            </div>                            
-                        </div>
-                    </div>
-                </div>
-            </div>
-        ) : isForTenants ? (
-            <div className="col-lg-4">
-                <div className="listing-cards for-request pt-4">
-                    <div className="listing-info for-request">
-                        <div className="title-group">
-                            <div className="listing-title mb-3">{property.name}</div>
+                            <div className="listing-title mb-3">{data.name}</div>
                         </div>
                         <div className="feature-group">
                             <div className="feature-sing w-100">
@@ -158,15 +49,14 @@ export default function SessionsCard({ property = {}, }) {
                     {/* <div className="line" /> */}
                     <div className="listing-info pt-0">
                         <div className="listing-btn">
-                            <button className="list-no-color-btn w-100" onClick={openTenancyDetails}> 
+                            <button className="list-no-color-btn w-100"> 
                                 View Details
                             </button>
                         </div>
                     </div>
                 </div>
             </div>
-        ) : null}
-    </>
+        </>
 
     )
 }
--- src/Pages/Clean/index.js
+++ src/Pages/Clean/index.js
@@ -1,12 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Wrapper } from "./Clean.styles";
 import Modal from "../../Utilities/Modal";
 import Fetch from "../../Utilities/Fetch";
 import Application from "../../Components/Generics/Form/Application";
 import { Box } from "@material-ui/core";
+import SessionsCard from "../../Components/Generics/SessionsCard";
 
 const Clean = () => {
     const [openModal, setOpenModal ] = useState(false);
+    const [loading, setLoading ] = useState(false);
+    const [offset, setOffset ] = useState(0);
+    const [limit, setLimit ] = useState(25);
+    const [requests, setRequest ] = useState([]);
+    
+    const fetchCleanRequest = async () => {
+        setLoading(true)
+        const requestData = {
+            limit: limit,
+            requests: requests
+        }
+        try {
+            setLoading(true)
+            let data  = await Fetch(`Clean/requests/user?offset=${offset}&limit=${limit}`)
+            // data = data.data.json();
+            console.log('Clean req: ', data)
+            setRequest(data.data.value)
+            setLoading(false)
+        } catch (error) {
+            setLoading(false)
+            // console.log( error)
+        }
+    }
+    
+    useEffect(() => {
+        fetchCleanRequest()
+    }, [])
     
     
     return (
@@ -39,11 +67,18 @@ const Clean = () => {
                         <div className="mt-4">
                             <h5>My Requests</h5>
                         </div>
+                        { requests.length !== 0
+                            ? requests.map((request, index) => {
+                                return (
+                                    <SessionsCard key={index} data={request} />
+                                )
+                            })
                         
-                        <Box display="flex" flexDirection="column" width="100%" alignItems="center" className="mt-3">
-                            <div className="iconsection mb-3" />
-                            <span>You currently have no  Cleaning sessions Booked.</span>
-                        </Box>
+                            : <Box display="flex" flexDirection="column" width="100%" alignItems="center" className="mt-3">
+                                <div className="iconsection mb-3" />
+                                <span>You currently have no  Cleaning sessions Booked.</span>
+                            </Box>
+                        }
                     </div>
                 </div>  
             </Wrapper>
